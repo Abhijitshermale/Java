4317bf4099b538b2533880e8234d9a610f8ec279
Abhijit <shermaleabhijit99@gmail.com>
2022-11-01 10:34:22 +0530

    Runtime polymorphism

---
+++ oop/RuntimePoly.java
@@ -0,0 +1,23 @@
+class RuntimePoly
+{
+	void show(String a, int b)
+	{
+		System.out.println(a+" "+b);
+	}
+}
+class B extends RuntimePoly
+{
+	void show(String a,int b)
+	{
+		System.out.println(a+" "+b);
+	}
+	public static void main(String[] atrgs)
+	{
+		B b=new B();
+		b.show("Abhi",777);
+
+		RuntimePoly rp=new RuntimePoly();
+		rp.show("Champ",999);
+	}
+}
+
